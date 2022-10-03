594452027e14248d47fe4cc41aa2465a7dc90d0a
Chloe Fugle <chloe.m.fugle.23@dartmouth.edu>
2022-10-03 11:01:20 -0400

    Add convert images to video

--- frames_to_video.py
+++ frames_to_video.py
@@ -1,3 +1,43 @@
 # Chloe Fugle (chloe.m.fugle.23@dartmouth.edu)
 # 10/2/2022
 # Bio97 Thesis Project
+# Writes frames to an mp4 video file using OpenCV2, code modified from code at https://stackoverflow.com/questions/43048725/python-creating-video-from-images-using-opencv
+
+import cv2
+
+# writes frames to an mp4 video file
+# inputs: file_path - path of outputted video, must end with .mp4
+#         frames - list of images
+#         fps - desired frame rate
+# output: mp4 video at specified path
+def write_video(file_path, frames, fps):
+    # read images to cv2 images and add them to list
+    im_list = []
+    for image in frames:
+        im_list.append(cv2.imread(image))
+
+    # get dimensions of image
+    h,w,l = im_list[0].shape
+
+    # initialize video writer
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    writer = cv2.VideoWriter(file_path, fourcc, fps, (w, h))
+
+    # write video
+    for frame in im_list:
+        writer.write(frame)
+
+    writer.release()
+
+# unit testing
+if __name__ == "__main__":
+    import os
+
+    # png file
+    FOLDER = ".npy test_1"
+    frame_list = []
+    for filename in os.scandir(FOLDER):
+        if filename.is_file() and os.path.splitext(filename)[1] == ".png":
+            frame_list.append(filename.path)
+
+    write_video("video_unit_test.mp4", frame_list, 1)
--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import numpy as np
 import cv2
 import os
 import load_npy
+import frames_to_video
 
 # folder
 FOLDER_NAME = ".npy test_1"
+VIDEO_FPS = 1
 
 tracker = cv2.TrackerKCF_create()
 # tracker = cv2.legacy.TrackerMOSSE_create()
@@ -24,60 +26,66 @@ for filename in os.scandir(video_folder):
         img_list.append(filename.path)
 
 # check to make sure all images in tiff folder are valid
+png_list = []
 for npy in img_list:
     png = load_npy.load(npy)
     if not png:
         print("Error: .npy " + npy + " could not be converted to a PNG. "
                                      "Please check the format and restart the program.")
-    img = cv2.imread(png)
+        break
+    png_list.append(png)
     print(png + " loaded")
 
-# tracking
-video = cv2.VideoCapture("walking.mp4")
-ret, frame = video.read()
+# convert list of images to mp4 video
+video_file_path = FOLDER_NAME + "/cell_tracker_video.mp4"
+frames_to_video.write_video(video_file_path, png_list, VIDEO_FPS)
 
-frame_height, frame_width = frame.shape[:2]
-# Resize the video for a more convinient view
-frame = cv2.resize(frame, [frame_width // 2, frame_height // 2])
-# Initialize video writer to save the results
-output = cv2.VideoWriter(f'{tracker_type}.avi',
-                         cv2.VideoWriter_fourcc(*'XVID'), 60.0,
-                         (frame_width // 2, frame_height // 2), True)
-if not ret:
-    print('cannot read the video')
-
-# Select the bounding box in the first frame
-bbox = cv2.selectROI(frame, False)
-ret = tracker.init(frame, bbox)
-
-# Start tracking
-while True:
-    ret, frame = video.read()
-    frame = cv2.resize(frame, [frame_width // 2, frame_height // 2])
-    if not ret:
-        print('something went wrong')
-        break
-    timer = cv2.getTickCount()
-    ret, bbox = tracker.update(frame)
-    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-    if ret:
-        p1 = (int(bbox[0]), int(bbox[1]))
-        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-        cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-    else:
-        cv2.putText(frame, "Tracking failure detected", (100, 80),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-    cv2.putText(frame, tracker_type + " Tracker", (100, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
-    cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
-    cv2.imshow("Tracking", frame)
-    output.write(frame)
-    k = cv2.waitKey(1) & 0xff
-    if k == 27: break
-
-video.release()
-output.release()
-cv2.destroyAllWindows()
+# # tracking
+# video = cv2.VideoCapture("walking.mp4")
+# ret, frame = video.read()
+#
+# frame_height, frame_width = frame.shape[:2]
+# # Resize the video for a more convinient view
+# frame = cv2.resize(frame, [frame_width // 2, frame_height // 2])
+# # Initialize video writer to save the results
+# output = cv2.VideoWriter(f'{tracker_type}.avi',
+#                          cv2.VideoWriter_fourcc(*'XVID'), 60.0,
+#                          (frame_width // 2, frame_height // 2), True)
+# if not ret:
+#     print('cannot read the video')
+#
+# # Select the bounding box in the first frame
+# bbox = cv2.selectROI(frame, False)
+# ret = tracker.init(frame, bbox)
+#
+# # Start tracking
+# while True:
+#     ret, frame = video.read()
+#     frame = cv2.resize(frame, [frame_width // 2, frame_height // 2])
+#     if not ret:
+#         print('something went wrong')
+#         break
+#     timer = cv2.getTickCount()
+#     ret, bbox = tracker.update(frame)
+#     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+#     if ret:
+#         p1 = (int(bbox[0]), int(bbox[1]))
+#         p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+#         cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+#     else:
+#         cv2.putText(frame, "Tracking failure detected", (100, 80),
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+#     cv2.putText(frame, tracker_type + " Tracker", (100, 20),
+#                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+#     cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50),
+#                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+#     cv2.imshow("Tracking", frame)
+#     output.write(frame)
+#     k = cv2.waitKey(1) & 0xff
+#     if k == 27: break
+#
+# video.release()
+# output.release()
+# cv2.destroyAllWindows()
 
 
